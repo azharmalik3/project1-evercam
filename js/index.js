f138e7b1569399515e36f7462fa3a6b737f55d66
javict <6javierct93@gmail.com>
2018-01-19 10:01:33 +0000

    new index.html design and share buttons

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var $btnWhatsapp = $('#btn-whatsapp');
+
+function base64ToBlob(data) {
+    var mimeString = '';
+    var raw, uInt8Array, i, rawLength;
+
+    raw = data.replace(rImageType, function(header, imageType) {
+        mimeString = imageType;
+
+        return '';
+    });
+
+    raw = atob(raw);
+    rawLength = raw.length;
+    uInt8Array = new Uint8Array(rawLength); // eslint-disable-line
+
+    for (i = 0; i < rawLength; i += 1) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], {type: mimeString});
+}
+
+$btnwhatsapp.on('click', function() {
+    var imageName = imageEditor.getImageName();
+    var dataURL = imageEditor.toDataURL();
+	var pathName = window.location.assign("/img/build.jpg");
+    var blob, type, w;
+
+    if (supportingFileAPI) {
+        blob = base64ToBlob(dataURL);
+        type = blob.type.split('/')[1];
+        if (imageName.split('.').pop() !== type) {
+            imageName += '.' + type;
+        }
+
+        // Library: FileSaver - saveAs
+        saveAs(blob, imageName); // eslint-disable-line
+    } else {
+        alert('This browser needs a file-server');
+        w = window.open();
+        w.document.body.innerHTML = '<img src=' + dataURL + '>';
+    }
+});
