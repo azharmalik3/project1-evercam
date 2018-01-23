ce809a8f9a7b95697f889db79eefd3cb4dc3a066
javict <6javierct93@gmail.com>
2018-01-23 11:50:11 +0000

    meta

--- image-editor/js/img.js
+++ image-editor/js/img.js
@@ -1,9 +1,10 @@
 function myfunction(){
 	var link = document.createElement('meta');
-	var link2 = document.createElement('meta');
-	link2.property="og:url";
-	link2.content=document.location;
-	link.property="og:title";
+	link.name="og:title";
 	link.content="Hola que pasa";
+	document.getElementsByTagName('head')[0].appendChild(link);
+	link = document.createElement('meta');
+	link.name="og:url";
+	link.content="https://project1-evercam.herokuapp.com/image-editor/img.html";
 	document.getElementsByTagName('head')[0].appendChild(link);
 }
