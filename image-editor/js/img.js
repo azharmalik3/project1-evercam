6d6d5a2551cea539f42a92dc1ffd87c6cb97bc71
javict <6javierct93@gmail.com>
2018-01-23 11:46:41 +0000

    meta

--- image-editor/js/img.js
+++ image-editor/js/img.js
@@ -1,9 +1,9 @@
 function myfunction(){
 	var link = document.createElement('meta');
 	var link2 = document.createElement('meta');
-	link2.name="og:url";
+	link2.property="og:url";
 	link2.content=document.location;
-	link.name="og:title";
+	link.property="og:title";
 	link.content="Hola que pasa";
 	document.getElementsByTagName('head')[0].appendChild(link);
 }
