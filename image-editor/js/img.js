1bec3519be74c7845cdd3613b1236b7b32054359
javict <6javierct93@gmail.com>
2018-01-23 11:44:13 +0000

    meta

--- image-editor/js/img.js
+++ image-editor/js/img.js
@@ -1,7 +1,8 @@
 function myfunction(){
 	var link = document.createElement('meta');
-	link.name="og:url";
-	link.content=document.location;
+	var link2 = document.createElement('meta');
+	link2.name="og:url";
+	link2.content=document.location;
 	link.name="og:title";
 	link.content="Hola que pasa";
 	document.getElementsByTagName('head')[0].appendChild(link);
