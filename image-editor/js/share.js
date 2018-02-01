6cc7ccd6d6ab0703da1e5b0778beb4a8654e40c9
javict <6javierct93@gmail.com>
2018-02-01 13:44:33 +0000

    cookies

--- image-editor/js/share.js
+++ image-editor/js/share.js
@@ -1,12 +1,32 @@
 var $whatsapp = $('#whatsapp');
 $whatsapp.on('click',function(e){
-	bannerImage = document.getElementById('miimagen');
-	newData = convertCanvasToImage(bannerImage);
-	imgData = getBase64Image(newData);
+	var bannerImage = document.getElementById('miimagen');
+	var newData = convertCanvasToImage(bannerImage);
+	var imgData = getBase64Image(newData);
   var dataImage = localStorage.getItem('imgData');
-	bannerImg = document.getElementById('myimgage');
-	bannerImg.src = 'data:image/png;base64,' + dataImage;
+	var bannerImg = document.getElementById('myimgage');
+	//bannerImg.src = 'data:image/png;base64,' + dataImage;
+  Cookies.set('name', 'data:image/png;base64,' + dataImage);
 });
+
+function convertCanvasToImage(canvas) {
+	var image = new Image();
+	image.src = imageEditor.toDataURL("image/png");
+	return image;
+}
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = 800;
+    canvas.height = 500;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, 800, 500);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
 
 /*var bannerImg;
 var bannerImage;
