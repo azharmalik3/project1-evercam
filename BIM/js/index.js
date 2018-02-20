4977629d733e2fbef02fd73b582c4202f455bd63
javict <6javierct93@gmail.com>
2018-02-20 16:45:38 +0000

    :fire: BIM model don't work. :smile: Adjust Image BIM model

--- BIM/js/index.js
+++ BIM/js/index.js
@@ -1,1 +1,37 @@
 registerAframeClickDragComponent(window.AFRAME);
+
+$(document).ready(function () {
+  $('#btn-object').click( function(e){
+		function makeid() {
+		  var text = "";
+		  var possible = "abcdefghijklmnopqrstuvwxyz";
+
+		  for (var i = 0; i < 5; i++)
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+		  return text;
+		}
+		var texto = makeid();
+		console.log(texto);
+
+		var sceneEl = document.querySelector('#target');
+		AFRAME.registerComponent(texto, {
+		  init: function () {
+			// This will be called after the entity has properly attached and loaded.
+			console.log('I am ready!');
+		  }
+		});
+		var entityEl = document.createElement('a-plane');
+		/*entityEl.setAttribute('geometry', {
+		  primitive: 'sphere',
+			radius: 1.25,
+			color: '#FFFFFF'
+		});*/
+		entityEl.setAttribute('radius', '1.25');
+		entityEl.setAttribute('color', '#FFFFFF');
+		entityEl.setAttribute('position', '0 1.25 -5');
+		sceneEl.appendChild(entityEl);
+		entityEl.addState('selected');
+		console.log("añadido");
+	});
+});
