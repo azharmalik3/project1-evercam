fc0c32c5283d3335a6147ffe517e560c841a2a27
javict <6javierct93@gmail.com>
2018-03-07 17:40:32 +0000

    :smile: Player working without stop and pause

--- BIMOverlay/bimOverlay.js
+++ BIMOverlay/bimOverlay.js
@@ -1,10 +1,20 @@
+var photo;
+var image = 1;
 $(document).ready(function() {
     var esto = $( "#myFigure" ).width();
     imgD.style.width = esto+"px";
+
+    var myPlayer = $( "#player" ).width();
+    var player = (esto / 2) - myPlayer;
+    document.getElementById("player").style.left = player + "px";
+
     $(window).bind('resize', function() {
       esto = $( "#myFigure" ).width();
       imgD.style.width = esto+"px";
+      player = (esto / 2) - myPlayer;
+      document.getElementById("player").style.left = player + "px";
     });
+
     var angle = 0;
     var margin = 0;
     document.getElementById("redo").onclick = function(){
@@ -47,11 +57,6 @@ function moveDivisor() {
 
 }
 
-function myFunction(){
-  var photo = "img/bim/" + $( "#sel1" ).val() + ".png";
-  $("#imgD").attr("src",photo);
-}
-
 function point_it(event){
 	pos_x = event.offsetX?(event.offsetX):event.pageX-document.getElementById("comparison").offsetLeft;
 	pos_y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("comparison").offsetTop;
@@ -59,4 +64,50 @@ function point_it(event){
 	document.getElementById("myFigure").style.top = (pos_y-15) ;
 	document.getElementById("myFigure").style.visibility = "visible" ;
   console.log("y: " + pos_y +", x: " + pos_x);
+}
+
+function nextPhoto(){
+  if(image < 20){
+    image = image + 1;
+    photo = "img/bim/" + image + ".png";
+    $("#imgD").attr("src",photo);
+  }else{
+    alert("No more BIM");
+  }
+}
+
+function prevPhoto(){
+  if(image > 1){
+    image = image - 1;
+    photo = "img/bim/" + image + ".png";
+    $("#imgD").attr("src",photo);
+  }else{
+    alert("No more BIM");
+  }
+}
+
+function play(){
+  for(var i = 1; i < 21; i++){
+      image = i;
+      doSetTimeout(i);
+  }
+}
+
+function doSetTimeout(i){
+  setTimeout(function(){
+    photo = "img/bim/" + i + ".png";
+    $("#imgD").attr("src",photo);
+  }, i * 500);
+}
+
+function firstBim(){
+  image = 1;
+  photo = "img/bim/" + image + ".png";
+  $("#imgD").attr("src",photo);
+}
+
+function lastBim(){
+  image = 20;
+  photo = "img/bim/" + image + ".png";
+  $("#imgD").attr("src",photo);
 }
